dbe233989fcde0fe88db7c77bcd0a028abf78b32
alishapckg <alishaproton@proton.me>
2026-06-05 16:47:54 +0300

    Initial Commit

---
+++ cursorpet/cursorpetApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursorpetApp.swift
+//  cursorpet
+//
+//  Created by NS on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct cursorpetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
